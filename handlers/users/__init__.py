606b6fddf941131c296110507ac11098aeff995f
OverNiko <Slai100@yandex.ru>
2023-06-28 14:27:38 +0500

    fix

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,5 +2,4 @@ from . import help
 from . import start
 from . import echo
 from . import cancel
-from . import pars_os
 from . import pogoda
--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -11,8 +11,8 @@ from loader import dp
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
     if {i.lower().translate(str.maketrans("", "", string.punctuation)) for i in message.text.split(" ")}\
-        .intersection(set(json.load(open("cenz.json")))) != set():
-        await message.reply("Маты запрещены!")
+        .intersection(set(json.load(open("D:\\Programming\\Python\\Python\\Bots\\Tg_Bots\\TG_BOT\\TG_BOT\\handlers\\users\\cenz.json")))) != set():
+        await message.reply("Веди себя прилично!")
         await message.delete()
     else:
         await message.answer(f"Копирую:\n"
